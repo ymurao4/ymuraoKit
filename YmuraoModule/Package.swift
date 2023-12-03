ce9df2cc49e3f460385d7190b907ceefbca76956
村尾慶伸 <yoshinobu612@icloud.com>
2023-12-03 19:24:23 +0900

    コメント削除

--- YmuraoModule/Package.swift
+++ YmuraoModule/Package.swift
@@ -9,14 +9,11 @@ let package = Package(
         .iOS(.v17),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: .ymuraoModule,
             targets: [.ymuraoModule]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: .ymuraoModule,
             dependencies: [
