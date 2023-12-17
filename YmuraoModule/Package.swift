46422b1ee51736c83c0d450309e170600419f6df
村尾慶伸 <yoshinobu612@icloud.com>
2023-12-17 15:52:56 +0900

    import swiftAlgorithms

--- YmuraoModule/Package.swift
+++ YmuraoModule/Package.swift
@@ -13,6 +13,9 @@ let package = Package(
             name: .ymuraoModule,
             targets: [.ymuraoModule]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
+    ],
     targets: [
         .target(
             name: .ymuraoModule,
@@ -27,7 +30,8 @@ let package = Package(
         .target(
             name: .views,
             dependencies: [
-                .asset
+                .asset,
+                .swiftAlgorithms
             ]
         )
     ]
@@ -37,9 +41,13 @@ extension String {
     static let ymuraoModule = "YmuraoModule"
     static let views = "Views"
     static let asset = "Asset"
+    // external package
+    static let swiftAlgorithms = "swift-algorithms"
 }
 
 extension Target.Dependency {
     static let views = Self(stringLiteral: .views)
     static let asset = Self(stringLiteral: .asset)
+    // external package
+    static let swiftAlgorithms = Self(stringLiteral: .swiftAlgorithms)
 }
